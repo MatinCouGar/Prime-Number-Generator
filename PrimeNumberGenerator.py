2dd5f0c9bfb509b7a6a9972085af6a39a423bb30
MatinCouGar <matin.a.gamer1@gmail.com>
2026-02-04 17:55:37 +0330

    start

---
+++ PrimeNumberGenerator.py
@@ -0,0 +1,24 @@
+def avval(a):
+    if a == 2:
+        return True
+    elif a == 1:
+        return False
+    if a%2 == 0:
+        return False
+    else:
+        runner = a//2 + 1
+        while runner != 2:
+            runner = runner -1
+            if a%runner == 0:
+                return False
+            else:
+                if runner == 2:
+                    return True
+addad = int(input("Generate up to what number? "))
+counter = 0
+while counter < addad:
+    if avval(counter) == True:
+        print(counter)
+        counter = counter + 1
+    else:
+        counter = counter + 1
